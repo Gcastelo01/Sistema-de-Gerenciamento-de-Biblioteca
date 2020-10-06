9cec4f7a48158d86e6bf8b7717ae3bc6fc030529
Gabriel Castelo Branco <65787071+Gcastelo01@users.noreply.github.com>
2020-10-06 17:38:40 -0300

    Add files via upload

---
+++ Projeto_de_Sistema_de_Biblioteca/funçoes/Json_Processor.py
@@ -0,0 +1,17 @@
+import json
+import meus_arquivos_pessoais.Projeto_de_Sistema_de_Biblioteca.PathFinder as pf
+
+
+def db_log_retriver():
+    pf.lib_installer()
+    log = pf.path_invoke()
+
+    with open(log['DB_logger'], 'r') as p_json:
+        return json.load(p_json)
+
+
+def img_retrivre():
+
+    pf.lib_installer()
+    log = pf.path_invoke()
+    return log['Img_dir']
