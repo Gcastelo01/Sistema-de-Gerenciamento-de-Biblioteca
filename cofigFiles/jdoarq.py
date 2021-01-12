e32d65a27587758d340ea4f4104104cf9d5504d6
Gabriel <gcastelo.rocha@gmail.com>
2021-01-11 22:43:25 -0300

    Inicio da refatoração

--- cofigFiles/jdoarq.py
+++ cofigFiles/jdoarq.py
@@ -3,4 +3,11 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-class Window(Gtk.Window)
+
+class LoginScreen(Gtk.Window):
+    def __init__(self):
+
+        Gtk.Window.__init__(self, title='Tela de Login', default_width=480, default_heigth=690)
+        
+        self.box = Gtk.Box(fill=True)
+        self.add
--- cofigFiles/libfunctions/libFunctions.py
+++ cofigFiles/libfunctions/libFunctions.py
@@ -15,6 +15,71 @@ user.execute('select * from login')
 login_sistema = user.fetchall()
 
 
+
+class Livro():
+    
+    def cadastro_de_livro(self, title, aut, edt, anp: int, edi, idi, ac: int, vol: int, rs, numpag: int, edc, img: str):
+
+        """Ligação com o banco de dados, enviando os dados do novo livro recém cadastrado dentro da tela de cadastro"""
+
+        user.execute(f"INSERT INTO LIVROS VALUES('{title}', '{aut}', '{edt}', {anp}, {edi}, '{idi}', {ac}, {vol}, {rs}, "
+                    f"{numpag}, '{edc}', 'E','{img}')")
+        cnx.commit()
+
+
+    def estado(self, titulo):
+        
+        user.execute(f"SELECT * FROM EMPRESTADOS WHERE TÍTULO LIKE %{titulo}%;")
+        __result = user.fetchall()
+
+        return __result
+
+
+    def emprestar(self, titulo, codigo, nome_do_credor, data_de_emprestimo, data_de_dev):
+        try:
+            estado = self.estado(titulo)
+
+        except titulo == '':
+            "Popup de 'Por favor insira um título'"
+
+        if estado == 'E':
+            return False
+
+        else:
+            if nome_do_credor != '' and data_de_emprestimo != '':
+                    data = str(data_de_emprestimo).replace('/', '')
+                    datadev = (str(data_de_dev).replace('/', ''))
+
+                    user.execute(f"INSERT INTO EMPRESTADOS VALUES('{nome_do_credor}', '{titulo}', {codigo},"
+                                 f"{data}, {datadev})")
+
+                    user.execute(F"UPDATE LIVROS SET STATUS_DO_LIVRO = 'E' WHERE TÍTULO = '{titulo}'")
+                    cnx.commit()
+
+    def receber(self, titulo):
+        try:
+            __estado = self.estado(titulo)
+
+        except titulo == '':
+            "Popup de 'Por favor insira um título'"
+
+        if estado == 'E':
+            return False
+        else:
+            if nome_do_credor != '' and data_de_emprestimo != '':
+                    data = str(data_de_emprestimo).replace('/', '')
+                    datadev = (str(data_de_dev).replace('/', ''))
+
+                    user.execute(f"INSERT INTO EMPRESTADOS VALUES('{nome_do_credor}', '{titulo}', {codigo},"
+                                 f"{data}, {datadev})")
+
+                    user.execute(F"UPDATE LIVROS SET STATUS_DO_LIVRO = 'E' WHERE TÍTULO = '{titulo}'")
+                    cnx.commit()
+
+
+
+
+
 def logsis(log, sen):
 
     """Responsável por verificar se o usuário digitou um usuário válido e sua senha correspondente"""
@@ -38,14 +103,6 @@ def cadastro_db(log, sen):
         cnx.commit()
         return True
 
-
-def cadastro_de_livro(title, aut, edt, anp: int, edi, idi, ac: int, vol: int, rs, numpag: int, edc, img: str):
-
-    """Ligação com o banco de dados, enviando os dados do novo livro recém cadastrado dentro da tela de cadastro"""
-
-    user.execute(f"INSERT INTO LIVROS VALUES('{title}', '{aut}', '{edt}', {anp}, {edi}, '{idi}', {ac}, {vol}, {rs}, "
-                 f"{numpag}, '{edc}', 'E','{img}')")
-    cnx.commit()
 
 
 def multiplos_resultados(results: list):
@@ -96,108 +153,6 @@ def busca_de_tags(tag):
 
     Função para buscar livros na lista através de #
 """
-
-
-def emprestados():
-
-    """"""
-
-    user.execute('SELECT NOME_DO_LIVRO FROM EMPRESTADOS')
-    empres = user.fetchall()
-    if len(empres) == 0:
-        sg.PopupOK('Não há livros emprestados!')
-    else:
-        layout = [[sg.Text('Selecione o Titulo emprestado do qual deseja \n'
-                           'ver informações: ', font=('timesnewroman', 10))],
-                  [sg.Text('', key='al', text_color='darkred', font=('timesnewroman', 10), size=(40, 1))],
-                  [sg.Combo(empres, readonly=True, size=(50, 1), key='n'), sg.Button('Confirma', key='c')]]
-
-        window = sg.Window('Livros emprestados', layout)
-        while True:
-            events, values = window.Read()
-            if events is None:
-                window.Close()
-                break
-            if values['n'] == '':
-                window['al'].Update('Nenhum Livro selecionado!')
-                window['al']('Nenhum Livro selecionado!')
-            elif events == 'c':
-                user.execute(f"SELECT * FROM EMPRESTADOS WHERE NOME_DO_LIVRO = '{values['n'][0]}'")
-                intel = user.fetchall()
-                layout2 = [[sg.Text('Com quem Está: '),
-                            sg.Text(f'{intel[0][0]}', size=(30, 1), background_color='darkblue',
-                                    text_color='yellow')],
-
-                           [sg.Text('Nome do Livro: '),
-                            sg.Text(f'{intel[0][1]}', size=(30, 1), background_color='darkblue',
-                                    text_color='yellow')],
-
-                           [sg.Text('Código do Livro: '),
-                            sg.Text(F'{intel[0][2]}', size=(30, 1), background_color='darkblue',
-                                    text_color='yellow')],
-
-                           [sg.Text('Data de Empréstimo: '),
-                            sg.Text(f'{intel[0][3][:2]}/{intel[0][3][2:4]}/{intel[0][3][4:]}',
-                                    size=(30, 1), background_color='darkblue',
-                                    text_color='yellow')],
-
-                           [sg.Text('Data de Devolução: '),
-                            sg.Text(f'{intel[0][4][0:2]}/{intel[0][4][2:4]}/{intel[0][4][4:]}',
-                                    size=(30, 1), background_color='darkblue',
-                                    text_color='yellow')],
-
-                           [sg.Button('OK', key='o', button_color=('white', 'green'))]]
-
-                window2 = sg.Window(f'{intel[0][1]}', layout2, element_justification='right')
-                events2, values2 = window2.Read()
-                if events2 in (None, 'o'):
-                    window2.Close()
-                    break
-
-
-def emprestar_um_livro():
-
-    """Tela responsável por fazer o empréstimo dos livros"""
-
-    layout = [[sg.Text('Nome do Livro: '), sg.InputText(key='title')],
-              [sg.Text('Quem está Recebendo: '), sg.InputText(key='receptor')],
-              [sg.Text('Data de empréstimo: '), sg.InputText(key='dataemp')],
-              [sg.Text('Data de devolução: '), sg.InputText(key='datadev')],
-              [sg.Button('Confirmar', key='conf'), sg.Button('Cancelar', key='cancel')]]
-
-    window = sg.Window('Empréstimo', layout, text_justification='left', element_justification='right')
-
-    código = 00000
-    user.execute('SELECT TÍTULO FROM LIVROS')
-    meus_lvros = user.fetchall()
-
-    while True:
-        event, values = window.Read()
-        print(values['title'])
-        user.execute(f"SELECT STATUS_DO_LIVRO FROM LIVROS WHERE TÍTULO = '{values['title']}'")
-        check = user.fetchall()
-        if event in (None, 'cancel'):
-            window.Close()
-            break
-        if event == 'conf':
-            if check[0][0] == 'E':
-                sg.PopupOK('Título já se encontra emprestado!')
-            elif values['title'] in meus_lvros[0]:
-                if values['title'] != '' and values['receptor'] != '' and values['dataemp'] != '':
-                    data = str(values['dataemp']).replace('/', '')
-                    datadev = (str(values['datadev']).replace('/', ''))
-                    user.execute(f"INSERT INTO EMPRESTADOS VALUES('{values['receptor']}', '{values['title']}', {código},"
-                                 f"{data}, {datadev})")
-                    user.execute(F"UPDATE LIVROS SET STATUS_DO_LIVRO = 'E' WHERE TÍTULO = '{values['title']}'")
-                    cnx.commit()
-                    sg.PopupOK('Livro emprestado!')
-                    window.Close()
-                    break
-                else:
-                    sg.PopupOK('Favor preencher todos os campos!')
-            else:
-                sg.PopupOK('O livro em questão não está registrado.')
-
 
 def recebimento():
 
