25cada16bd772789963bd6bcbac4127aee22c272
Gabriel <gcastelo.rocha@gmail.com>
2021-01-13 11:44:53 -0300

    Terminada refatoração de libfunctions.py

--- cofigFiles/libfunctions/libFunctions.py
+++ cofigFiles/libfunctions/libFunctions.py
@@ -1,4 +1,4 @@
-# A Base do programa d
+# A Base do programa 
 import mysql.connector as bd
 import PySimpleGUI as sg
 import Json_Processor as jp
@@ -7,30 +7,27 @@ livros = list()
 db_log = jp.db_log_retriver()
 img_dir = jp.img_retrivre()
 
-cnx = bd.connect(user=db_log['user'], password=db_log['password'], host=db_log['host'], auth_plugin=db_log['auth_plugin'])
-user = cnx.cursor()
-
-user.execute('USE APP_BIBLIOTECA')
-user.execute('select * from login')
-login_sistema = user.fetchall()
-
+#TODO: Fazer a função de recuperação dos múltiplos usuários, permitindo que ele escolha qual livro deseja ver.
 
 
 class Livro():
-    
+    def __init__(self):
+        self.cnx = bd.connect(user=db_log['user'], password=db_log['password'], host=db_log['host'], auth_plugin=db_log['auth_plugin'])
+        self.user = self.cnx.cursor()
+
     def cadastro_de_livro(self, title, aut, edt, anp: int, edi, idi, ac: int, vol: int, rs, numpag: int, edc, img: str):
 
         """Ligação com o banco de dados, enviando os dados do novo livro recém cadastrado dentro da tela de cadastro"""
 
-        user.execute(f"INSERT INTO LIVROS VALUES('{title}', '{aut}', '{edt}', {anp}, {edi}, '{idi}', {ac}, {vol}, {rs}, "
+        self.user.execute(f"INSERT INTO LIVROS VALUES('{title}', '{aut}', '{edt}', {anp}, {edi}, '{idi}', {ac}, {vol}, {rs}, "
                     f"{numpag}, '{edc}', 'E','{img}')")
-        cnx.commit()
+        self.cnx.commit()
 
 
     def estado(self, titulo):
         
-        user.execute(f"SELECT * FROM EMPRESTADOS WHERE TÍTULO LIKE %{titulo}%;")
-        __result = user.fetchall()
+        self.user.execute(f"SELECT * FROM EMPRESTADOS WHERE TÍTULO LIKE %{titulo}%;")
+        __result = self.user.fetchall()
 
         return __result
 
@@ -50,11 +47,11 @@ class Livro():
                     data = str(data_de_emprestimo).replace('/', '')
                     datadev = (str(data_de_dev).replace('/', ''))
 
-                    user.execute(f"INSERT INTO EMPRESTADOS VALUES('{nome_do_credor}', '{titulo}', {codigo},"
+                    self.user.execute(f"INSERT INTO EMPRESTADOS VALUES('{nome_do_credor}', '{titulo}', {codigo},"
                                  f"{data}, {datadev})")
 
-                    user.execute(F"UPDATE LIVROS SET STATUS_DO_LIVRO = 'E' WHERE TÍTULO = '{titulo}'")
-                    cnx.commit()
+                    self.user.execute(F"UPDATE LIVROS SET STATUS_DO_LIVRO = 'E' WHERE TÍTULO = '{titulo}'")
+                    self.cnx.commit()
 
     def receber(self, titulo):
         try:
@@ -63,119 +60,70 @@ class Livro():
         except titulo == '':
             "Popup de 'Por favor insira um título'"
 
-        if estado == 'E':
-            return False
+        if __estado == 'E':
+
+            self.user.execute(f"UPDATE LIVROS SET STATUS_DO_LIVRO = 'NE' WHERE TÍTULO = '{titulo}'")
+            self.cnx.commit()
+            self.user.execute(f"DELETE FROM EMPRESTADOS WHERE NOME_DO_LIVRO = '{titulo}'")
+            self.cnx.commit()
+
+    def procurarDB(self, titulo):
+        """Essa função é responsável por buscar dentro do banco de dados as informações referentes ao título do livro que
+        foi pesquisa"""
+
+        self.user.execute(f"SELECT TÍTULO FROM LIVROS WHERE TÍTULO LIKE '%{titulo}%'")
+        tanto = self.user.fetchall()
+
+        if len(tanto) > 1:
+            esc = self.multiplos_resultados(tanto)
+            self.user.execute(f"SELECT * FROM LIVROS WHERE TÍTULO = '{esc}'")
+            resultado = self.user.fetchall()
+            return resultado
+
         else:
-            if nome_do_credor != '' and data_de_emprestimo != '':
-                    data = str(data_de_emprestimo).replace('/', '')
-                    datadev = (str(data_de_dev).replace('/', ''))
+            self.user.execute(f"SELECT * FROM LIVROS WHERE TÍTULO LIKE '%{titulo}%'")
+            resultado = self.user.fetchall()
+            return resultado
 
-                    user.execute(f"INSERT INTO EMPRESTADOS VALUES('{nome_do_credor}', '{titulo}', {codigo},"
-                                 f"{data}, {datadev})")
 
-                    user.execute(F"UPDATE LIVROS SET STATUS_DO_LIVRO = 'E' WHERE TÍTULO = '{titulo}'")
-                    cnx.commit()
-
+    def multiplos_resultados(self, results: list):
+        # Aqui vai entrar a lógica por trás da seleção de multiplos resultados
+        return results[0] 
 
 
 
+class User():
 
-def logsis(log, sen):
-
-    """Responsável por verificar se o usuário digitou um usuário válido e sua senha correspondente"""
-
-    for v in login_sistema:
-        if v[0] == log and v[1] == sen:
-            return True
-    else:
-        return False
+    def __init__(self, username, password):
+        self.__cnx = bd.connect(user=db_log['user'], password=db_log['password'], host=db_log['host'], auth_plugin=db_log['auth_plugin'])
+        self. __user = self.__cnx.cursor()
+        self.__username = username
+        self.__password = password
 
 
-def cadastro_db(log, sen):
+    def __databaseCheck(self, username):
 
-    """Insere dados de um novo usuário caso ele não exista nos registros do banco de dados """
+        self.__user.execute('SELECT * FROM login')
+        return self.__user.fetchall()
+
+
+    def cadastro_db(self):
+
+        """Insere dados de um novo usuário caso ele não exista nos registros do banco de dados """
     
-    for v in login_sistema:
-        if v[0] == log:
+        for v in self.__databaseCheck(self.__username):
+            if v[0] == self.__username:
+                return False
+
+        else:
+            self.__user.execute(f"INSERT INTO LOGIN (USUÁRIO, SENHA) VALUES('{self.__username}', '{self.__password}')")
+            self.__cnx.commit()
+            return True
+    
+    def loginSist(self):
+    
+        for v in self.__databaseCheck(self.__username):
+            if v[0] == self.__username and v[1] == self.__password:
+                return True
+        else:
             return False
-    else:
-        user.execute(f"INSERT INTO LOGIN (USUÁRIO, SENHA) VALUES('{log}', '{sen}')")
-        cnx.commit()
-        return True
-
-
-
-def multiplos_resultados(results: list):
-
-    """Quando uma pesquisa no banco de dados retorna múltiplos resultados de títulos de livros, seja a pesquisa de
-    livros emprestados ou a lista total de livros cadastrada, esse popup permite ao usuário escolher qual dos títulos
-    deseja visualizar"""
-
-    layout = [[sg.Text('Multiplos resultdos encontrados. Selecione o que deseja vizualizar.')],
-              [sg.Combo(results, key='0', size=(50, 1), readonly=True, font=['bahnschriftsemiLightsemiconde', 8])],
-              [sg.Button('Ok', key='ok')]]
-
-    window = sg.Window('Multiplos Resultados', layout)
-
-    while True:
-        events, values = window.read()
-
-        if events is None:
-            window.Close()
-            break
-        if events == 'ok':
-            window.Close()
-            return values['0'][0]
-
-
-def pesquisa_no_bd(title):
-
-    """Essa função é responsável por buscar dentro do banco de dados as informações referentes ao título do livro que
-    foi pesquisa"""
-
-    user.execute(f"SELECT TÍTULO FROM LIVROS WHERE TÍTULO LIKE '%{title}%'")
-    tanto = user.fetchall()
-
-    if len(tanto) > 1:
-        esc = multiplos_resultados(tanto)
-        user.execute(f"SELECT * FROM LIVROS WHERE TÍTULO = '{esc}'")
-        resultado = user.fetchall()
-        return resultado
-
-    else:
-        user.execute(f"SELECT * FROM LIVROS WHERE TÍTULO LIKE '%{title}%'")
-        resultado = user.fetchall()
-        return resultado
-
-
-"""
-def busca_de_tags(tag):
-
-    Função para buscar livros na lista através de #
-"""
-
-def recebimento():
-
-    """Função responsável por retirar da tabela de emprestados e devolver à tabela principal"""
-
-    user.execute('SELECT NOME_DO_LIVRO FROM EMPRESTADOS')
-    emprestado = user.fetchall()
-    layout = [[sg.Text('Escolha o livro a ser recebido:')],
-              [sg.Combo(emprestado, key='rec', readonly=True)],
-              [sg.Button('Ok', key='ok'), sg.Button('Cancelar', key='can')]]
-
-    window = sg.Window('Recebimento', layout)
-
-    while True:
-        event, values = window.Read()
-        if event in (None, 'can'):
-            window.Close()
-            break
-        elif event == 'ok':
-            user.execute(f"UPDATE LIVROS SET STATUS_DO_LIVRO = 'NE' WHERE TÍTULO = '{values['rec'][0]}'")
-            cnx.commit()
-            user.execute(f"DELETE FROM EMPRESTADOS WHERE NOME_DO_LIVRO = '{values['rec'][0]}'")
-            cnx.commit()
-            sg.PopupOK('Livro devolvido à sua biblioteca!')
-            window.Close()
-            break
